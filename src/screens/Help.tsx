94698b88416a6827406e72686ef64a0c16d66a90
Linda Paiste <lindapaiste@gmail.com>
2020-08-21 15:15:31 -0500

    clean-up

--- src/screens/Help.tsx
+++ src/screens/Help.tsx
@@ -1,13 +1,11 @@
 import {Props} from "./Paused";
 import Title from "../components/Title";
 import Button from "../components/Button";
-import React, {useState} from "react";
+import React from "react";
 import Instructions from "../components/Instructions";
 import ScreenWrapper from "./ScreenWrapper";
 
 export default ({onPressResume}: Props) => {
-    const [isOpenPrivacy, setIsOpenPrivacy] = useState(false);
-
     return (
         <ScreenWrapper>
             <Title vw={8}>
